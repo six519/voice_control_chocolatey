57452e47de97d31b34ea214f7b87cda38c9a9eae
six519 <ferdinandsilva@ferdinandsilva.com>
2020-06-28 16:15:02 +0800

    first commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'Setup.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  file         = $fileLocation
+
+  softwareName  = 'voice_control*'
+  checksum      = '79d6933ccee30a7d6343371707b761078585260e4ec31e4fa6f24cdddd6cbdd5'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
